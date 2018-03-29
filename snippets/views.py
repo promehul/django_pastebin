656c64ba7391e289e43fc0949c73ca2af8ef3be8
harshit khetan <khetanmehul@gmail.com>
2018-03-30 00:15:22 +0530

    completed pastebin

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from snippets.forms import SnippetForm
+from django.http import HttpResponse,Http404
+from snippets.forms import SnippetForm, CommentForm
 from snippets.models import Snippet
 
 def create(request):
@@ -16,6 +16,22 @@ def create(request):
   return render(request, 'snippets/create.html',{'form':form})
 
 def view(request, key):
-  return HttpResponse("View function:" +str(key))
+ 
+  if Snippet.objects.filter(id=key).exists():
+    snippet = Snippet.objects.get(id=key)
+    
+  else:
+    raise Http404("Snippet doesn't exist")
+  
+  if request.method == "POST":
+    form = CommentForm(request.POST)
+    if form.is_valid():
+      comment = form.save(commit=False)    
+      comment.snippet = snippet
+      comment.save()
+  form = CommentForm()
+  comments = snippet.comment_set.all().order_by('-datetime_created')  
+
+  return render(request, 'snippets/view.html',{'key':key , 'title': snippet.title, 'content': snippet.content , 'form': form, 'comments': comments})
 
 
